7e3754a21af6d8163277956c998021b71a397377
John O'Neill <john.oneill@danfoss.com>
2019-08-19 11:21:55 -0500

    Barely working Front wheels go floppy tho. No idea why.

--- cv_barcode/scripts/make_qr_sim.sh
+++ cv_barcode/scripts/make_qr_sim.sh
@@ -22,7 +22,7 @@ SEARCH="RonnyLM"
 # so if using 2 digit numbers, QRPREFIX should be 8 or less characters to stay within the V1 size with high error correction.
 # See https://www.qrcode.com/en/about/version.html for more details on sizes.
 # Also, I (John) think this may be case insensitive, but I'm not certain.
-QRPREFIX="FitzA"
+QRPREFIX="FitzB"
 # How many tags to make
 NUMTAGS=9
 
@@ -36,7 +36,7 @@ do
 	rm -R -f $DESTPATH/$QRNAME
 
   # Copy the template to the new folder
-	cp -R ${SEARCHPATH}/${SEARCH} $DESTPATH/$QRNAME
+	#cp -R ${SEARCHPATH}/${SEARCH} $DESTPATH/$QRNAME
 
 	# Delete the old QR code
 	rm $DESTPATH/$QRNAME/materials/textures/${SEARCH}.png
@@ -44,14 +44,15 @@ do
 	# This generates a 168 px qrcode btw, 21*8=168 plus 2*8*2=200 for a 200px square image, with a 168 px qr in the middle.
   # Note that if QRNAME is more than 10 characters, this will bump size up to 25x25 changing the image size.
   # For best results, the smaller the barcode the better, as it will be readable from further away.
-	qrencode  -l H -s 8 -m 2 -o $DESTPATH/$QRNAME/materials/textures/${QRNAME}.png "${QRNAME}"
+	#qrencode  -l H -s 8 -m 2 -o $DESTPATH/$QRNAME/materials/textures/${QRNAME}.png "${QRNAME}"
+	qrencode  -l H -s 128 -m 2 -o $DESTPATH/${QRNAME}.png "${QRNAME}"
 
   # Rename the material file
-	mv $DESTPATH/$QRNAME/materials/scripts/${SEARCH}.material $DESTPATH/$QRNAME/materials/scripts/${QRNAME}.material
+	#mv $DESTPATH/$QRNAME/materials/scripts/${SEARCH}.material $DESTPATH/$QRNAME/materials/scripts/${QRNAME}.material
 
   # Search and replace for the name in all the relevant files
-	sed -i -e "s/${SEARCH}/${QRNAME}/g" "$DESTPATH/$QRNAME/materials/scripts/${QRNAME}.material"
-	sed -i -e "s/${SEARCH}/${QRNAME}/g" "$DESTPATH/$QRNAME/model.config"
-	sed -i -e "s/${SEARCH}/${QRNAME}/g" "$DESTPATH/$QRNAME/model.sdf"
+	#sed -i -e "s/${SEARCH}/${QRNAME}/g" "$DESTPATH/$QRNAME/materials/scripts/${QRNAME}.material"
+	#sed -i -e "s/${SEARCH}/${QRNAME}/g" "$DESTPATH/$QRNAME/model.config"
+	#sed -i -e "s/${SEARCH}/${QRNAME}/g" "$DESTPATH/$QRNAME/model.sdf"
 done
 
